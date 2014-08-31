a3278a8b41fb39fd1653049f0bfa4b5fd73cfc10
dyep49 <dyep49@gmail.com>
2014-08-31 02:07:51 -0400

    init commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express'),
+    app = express(),
+    childProcess = require('child_process');
+
+
+app.post('/', function(req, res) {
+  var secretHeader = req.headers['x-hub-signature']
+  var body = JSON.stringify(req.body)
+  if(secretCheck(body, secretHeader) === true) {
+    childProcess.exec('git pull', {cwd: './../apple-landing/'} function(err, stdout, stderr) {
+      console.log(stdout)
+      res.send(stdout)
+    })
+  } else {
+    res.send("Invalid request")
+  }
+});
+
+function secretCheck(body, hash) {
+  var crypto = require('crypto');
+  var hmac = crypto.createHmac('sha1', process.env.WEBHOOKS_SECRET)
+  hmac.write(body)
+  hmac.end()
+  return ("sha1=" + hmac.read().toString('hex')) === hash 
+}
